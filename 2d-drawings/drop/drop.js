3cb1f1ad89920413d1db9a30fc02e16d89f98313
olivia svensson <oliviateoliva@gmail.com>
2023-06-24 12:38:00 +0200

    Add artwork Drop

---
+++ 2d-drawings/drop/drop.js
@@ -0,0 +1,57 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function setup() {
+	createCanvas(1000,1000);
+
+}
+
+function draw() {
+		background(249,207,245);
+		
+fill(249,207,245);
+	ellipse(500,500,700,700);
+	strokeWeight(20);
+		stroke(200,0,100,40);
+	
+	
+	ellipse(500,500,600,600);
+	strokeWeight(10);
+		stroke(250,0,200,40);
+	
+	ellipse(500,500,500,500);
+	strokeWeight(20);
+	stroke(140,0,200,40);
+	
+	ellipse(500,500,400,400);
+	strokeWeight(30);
+		stroke(140,0,100,40);
+	
+	ellipse(500,500,300,300);
+	strokeWeight(5);
+	stroke(100,0,200,40);
+	
+	ellipse(500,500,200,200);
+	strokeWeight(20);
+		stroke(50,0,240,40);
+	
+		ellipse(500,500,100,100);
+	stroke(0,0,240,40);
+	strokeWeight(3);
+	
+		ellipse(500,500,100,100);
+	stroke(0,0,240,40);
+	strokeWeight(3);
+		ellipse(500,500,100,100);
+	stroke(0,0,240,40);
+	strokeWeight(3);
+		ellipse(500,500,100,100);
+	stroke(0,0,240,40);
+	strokeWeight(3);
+	
+	
+}
