074ce26c6e7ad3cdd73661a0c9d9cbe85bd9e79c
olivia svensson <oliviateoliva@gmail.com>
2023-06-24 12:19:45 +0200

    Add lemonade drawing

---
+++ 2d-drawings/lemonade.js
@@ -0,0 +1,177 @@
+function setup() {
+	createCanvas(1000,1000);
+}
+
+function draw() {
+		//background(38,28,120);
+//	background(255,146,223);	
+	background(186,228,255);
+	noStroke();
+	
+	
+	
+	//ORANGE RED
+	
+	fill(212,65,35,70);
+	arc(200,700, 300,300,0, HALF_PI);
+	
+	fill(212,65,35,70);
+	arc(200,700,300,300,QUARTER_PI, HALF_PI);
+	
+fill(255,94,0,70);
+	arc(200,700, 300,300,HALF_PI, HALF_PI);
+	
+	
+	fill(212,65,35,70);
+	arc(200,700,300,300,QUARTER_PI + HALF_PI, HALF_PI);
+	
+	fill(212,65,35,70);
+	arc(200,700, 300,300,PI, HALF_PI);
+	
+	
+	fill(212,65,35,70);
+	arc(200,700,300,300,PI +QUARTER_PI  , HALF_PI);
+	
+	
+	fill(212,65,35,70);
+	arc(200,700, 300,300,PI +HALF_PI  , HALF_PI);
+	
+	
+	fill(212,65,35,70);
+	arc(200,700, 300,300,PI +QUARTER_PI + HALF_PI  , HALF_PI);
+	
+	
+	fill(212,65,35,70);
+	arc(200,700, 300,300,TWO_PI  , HALF_PI);
+	
+	fill(212,65,35,70);
+	arc(200,700,300,300,TWO_PI  + HALF_PI  , HALF_PI);
+	
+	
+	
+	
+	//LIGHT YELLOW
+	
+	fill(255,230,0,70);
+	arc(800,700,  400,400,0, HALF_PI);
+	
+		fill(255,230,0,70);
+	arc(800,700, 400,400,QUARTER_PI, HALF_PI);
+	
+	fill(255,230,0,70);
+	arc(800,700, 400,400,HALF_PI, HALF_PI);
+	
+	
+	fill(255,230,0,70);
+	arc(800,700,  400,400,QUARTER_PI + HALF_PI, HALF_PI);
+	
+	fill(255,230,0,70);
+	arc(800,700, 400,400,PI, HALF_PI);
+	
+	
+		fill(255,230,0,70);
+	arc(800,700,  400,400,PI +QUARTER_PI  , HALF_PI);
+	
+	
+	
+	fill(255,230,0,70);
+	arc(800,700, 400,400,PI +HALF_PI  , HALF_PI);
+	
+	
+		fill(255,230,0,70);
+	arc(800,700,  400,400,PI +QUARTER_PI + HALF_PI  , HALF_PI);
+	
+	
+	fill(255,230,0,70);
+	arc(800,700,  400,400,TWO_PI  , HALF_PI);
+	
+	fill(255,230,0,70);
+	arc(800,700,  400,400,TWO_PI  + HALF_PI  , HALF_PI);
+	
+	
+	
+	
+	
+	//YELLOW
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,0, HALF_PI);
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,QUARTER_PI, HALF_PI);
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,HALF_PI, HALF_PI);
+	
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,QUARTER_PI + HALF_PI, HALF_PI);
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,PI, HALF_PI);
+	
+	
+fill(255,196,0,70);
+	arc(300,300, 500,500,PI +QUARTER_PI  , HALF_PI);
+	
+	
+	
+fill(255,196,0,70);
+	arc(300,300, 500,500,PI +HALF_PI  , HALF_PI);
+	
+	
+fill(255,196,0,70);
+	arc(300,300, 500,500,PI +QUARTER_PI + HALF_PI  , HALF_PI);
+	
+	
+	fill(255,196,0,70);
+	arc(300,300, 500,500,TWO_PI  , HALF_PI);
+	
+	
+fill(255,196,0,70);
+	arc(300,300, 500,500,TWO_PI  + HALF_PI  , HALF_PI);
+	
+	
+	
+	
+	
+	//ORANGE
+	fill(255,100,0,70);
+	arc(500,500, 500,500,0, HALF_PI);
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,QUARTER_PI, HALF_PI);
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,HALF_PI, HALF_PI);
+	
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,QUARTER_PI + HALF_PI, HALF_PI);
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,PI, HALF_PI);
+	
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,PI +QUARTER_PI  , HALF_PI);
+	
+	
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,PI +HALF_PI  , HALF_PI);
+	
+	
+	fill(255,196,0,70);
+	arc(500,500, 500,500,PI +QUARTER_PI + HALF_PI  , HALF_PI);
+	
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,TWO_PI  , HALF_PI);
+	
+	
+	fill(255,100,0,70);
+	arc(500,500, 500,500,TWO_PI  + HALF_PI  , HALF_PI);
+	
+
+}
