99026c499d2c1de7234bc1299438bafa7c278c4d
olivia svensson <oliviateoliva@gmail.com>
2023-08-09 16:50:02 +0200

    Add artwork Pink Drops

---
+++ 2d-drawings/Pink Drops/sketch.js
@@ -0,0 +1,328 @@
+function setup() {
+	createCanvas(500, 500);
+background(255, 255, 255);
+	 noStroke(); 
+}
+
+
+function draw() {
+let pinkk = color(255, 56, 73);
+let bluee = color(255, 75, 90);
+let redd = color(255, 93, 130);
+let yelloww = color(255, 151, 180);
+let orangee = color(255, 56, 73);
+let brownn = color(254, 120, 157);
+let greyy = color(255, 113, 124);
+	
+	//row 1
+		  fill(pinkk);
+	ellipse(25,25,50,50);
+	
+	fill(bluee);
+	ellipse(75,25,50,50);
+	
+	fill(redd);
+	ellipse(125,25,50,50);
+	
+	fill(yelloww);
+	ellipse(175,25,50,50);
+	
+	fill(orangee);
+	ellipse(225,25,50,50);
+	
+	fill(brownn);
+	ellipse(275,25,50,50);
+	
+	fill(greyy);
+	ellipse(325,25,50,50);
+	
+	 fill(pinkk);
+	ellipse(375,25,50,50);
+	
+	fill(bluee);
+	ellipse(425,25,50,50);
+	
+	fill(redd);
+	ellipse(475,25,50,50);
+	
+	//row 2
+		  fill(bluee);
+	ellipse(25,75,50,50);
+	
+	fill(redd);
+	ellipse(75,75,50,50);
+	
+	fill(yelloww);
+	ellipse(125,75,50,50);
+	
+	fill(bluee);
+	ellipse(175,75,50,50);
+	
+	fill(brownn);
+	ellipse(225,75,50,50);
+	
+	fill(yelloww);
+	ellipse(275,75,50,50);
+	
+	fill(bluee);
+	ellipse(325,75,50,50);
+	
+	 fill(greyy);
+	ellipse(375,75,50,50);
+	
+	fill(redd);
+	ellipse(425,75,50,50);
+	
+	fill(bluee);
+	ellipse(475,75,50,50);
+	
+		//row 3
+		  fill(greyy);
+	ellipse(25,125,50,50);
+	
+	fill(bluee);
+	ellipse(75,125,50,50);
+	
+	fill(redd);
+	ellipse(125,125,50,50);
+	
+	fill(yelloww);
+	ellipse(175,125,50,50);
+	
+	fill(orangee);
+	ellipse(225,125,50,50);
+	
+	fill(yelloww);
+	ellipse(275,125,50,50);
+	
+	fill(brownn);
+	ellipse(325,125,50,50);
+	
+	 fill(redd);
+	ellipse(375,125,50,50);
+	
+	fill(bluee);
+	ellipse(425,125,50,50);
+	
+	fill(redd);
+	ellipse(475,125,50,50);
+	
+	//row 4
+		  fill(pinkk);
+	ellipse(25,175,50,50);
+	
+	fill(bluee);
+	ellipse(75,175,50,50);
+	
+	fill(redd);
+	ellipse(125,175,50,50);
+	
+	fill(yelloww);
+	ellipse(175,175,50,50);
+	
+	fill(orangee);
+	ellipse(225,175,50,50);
+	
+	fill(brownn);
+	ellipse(275,175,50,50);
+	
+	fill(greyy);
+	ellipse(325,175,50,50);
+	
+	 fill(pinkk);
+	ellipse(375,175,50,50);
+	
+	fill(bluee);
+	ellipse(425,175,50,50);
+	
+	fill(redd);
+	ellipse(475,175,50,50);
+	
+	//row 5
+		//row 2
+		  fill(bluee);
+	ellipse(25,225,50,50);
+	
+	fill(redd);
+	ellipse(75,225,50,50);
+	
+	fill(yelloww);
+	ellipse(125,225,50,50);
+	
+	fill(bluee);
+	ellipse(175,225,50,50);
+	
+	fill(brownn);
+	ellipse(225,225,50,50);
+	
+	fill(yelloww);
+	ellipse(275,225,50,50);
+	
+	fill(bluee);
+	ellipse(325,225,50,50);
+	
+	 fill(greyy);
+	ellipse(375,225,50,50);
+	
+	fill(redd);
+	ellipse(425,225,50,50);
+	
+	fill(bluee);
+	ellipse(475,225,50,50);
+	
+	//row 6
+		  fill(greyy);
+	ellipse(25,275,50,50);
+	
+	fill(bluee);
+	ellipse(75,275,50,50);
+	
+	fill(redd);
+	ellipse(125,275,50,50);
+	
+	fill(yelloww);
+	ellipse(175,275,50,50);
+	
+	fill(orangee);
+	ellipse(225,275,50,50);
+	
+	fill(yelloww);
+	ellipse(275,275,50,50);
+	
+	fill(brownn);
+	ellipse(325,275,50,50);
+	
+	 fill(redd);
+	ellipse(375,275,50,50);
+	
+	fill(bluee);
+	ellipse(425,275,50,50);
+	
+	fill(redd);
+	ellipse(475,275,50,50);
+	
+	//row 7
+		  fill(greyy);
+	ellipse(25,325,50,50);
+	
+	fill(redd);
+	ellipse(75,325,50,50);
+	
+	fill(orangee);
+	ellipse(125,325,50,50);
+	
+	fill(bluee);
+	ellipse(175,325,50,50);
+	
+	fill(redd);
+	ellipse(225,325,50,50);
+	
+	fill(orangee);
+	ellipse(275,325,50,50);
+	
+	fill(bluee);
+	ellipse(325,325,50,50);
+	
+	 fill(yelloww);
+	ellipse(375,325,50,50);
+	
+fill(brownn);
+	ellipse(425,325,50,50);
+	
+	fill(yelloww);
+	ellipse(475,325,50,50);
+	
+	//row 8
+		  fill(pinkk);
+	ellipse(25,375,50,50);
+	
+	fill(bluee);
+	ellipse(75,375,50,50);
+	
+	fill(redd);
+	ellipse(125,375,50,50);
+	
+	fill(yelloww);
+	ellipse(175,375,50,50);
+	
+	fill(orangee);
+	ellipse(225,375,50,50);
+	
+	fill(brownn);
+	ellipse(275,375,50,50);
+	
+	fill(greyy);
+	ellipse(325,375,50,50);
+	
+	 fill(pinkk);
+	ellipse(375,375,50,50);
+	
+	fill(bluee);
+	ellipse(425,375,50,50);
+	
+	fill(redd);
+	ellipse(475,375,50,50);
+	
+	//row 9
+		  fill(greyy);
+	ellipse(25,425,50,50);
+	
+	fill(bluee);
+	ellipse(75,425,50,50);
+	
+	fill(redd);
+	ellipse(125,425,50,50);
+	
+	fill(yelloww);
+	ellipse(175,425,50,50);
+	
+	fill(orangee);
+	ellipse(225,425,50,50);
+	
+	fill(yelloww);
+	ellipse(275,425,50,50);
+	
+	fill(brownn);
+	ellipse(325,425,50,50);
+	
+	 fill(redd);
+	ellipse(375,425,50,50);
+	
+	fill(bluee);
+	ellipse(425,425,50,50);
+	
+	fill(redd);
+	ellipse(475,425,50,50);
+	
+	//row 10
+		  fill(redd);
+	ellipse(25,475,50,50);
+	
+	fill(greyy);
+	ellipse(75,475,50,50);
+	
+	fill(orangee);
+	ellipse(125,475,50,50);
+	
+	fill(bluee);
+	ellipse(175,475,50,50);
+	
+	fill(redd);
+	ellipse(225,475,50,50);
+	
+	fill(orangee);
+	ellipse(275,475,50,50);
+	
+	fill(bluee);
+	ellipse(325,475,50,50);
+	
+	 fill(yelloww);
+	ellipse(375,475,50,50);
+	
+fill(brownn);
+	ellipse(425,475,50,50);
+	
+	fill(yelloww);
+	ellipse(475,475,50,50);
+	
+}
